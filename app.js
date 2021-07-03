4ed55fbdbb45a0e9a6eba8039e312455e5a97015
cguirgu <cguirgu@ncsu.edu>
2021-07-02 21:59:48 -0400

    Takes address in from command line

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const geocode = require("./utils/geocode")
 const forecast = require("./utils/forecast")
 
-// When we call geocode, we will pass in the address and also the function that we
-// want to perform once we have the latitude and longitude; typically 2 arguments are passed
-// in the callback function (an error or the data if things went well)
-geocode("boston", (error, data) => {
-  console.log("Error:", error)
-  console.log("Data:", data)
-})
+const address = process.argv[2]
 
-// The forecast function should has three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-forecast(37.8367, -122.4233, (error, data) => {
-  console.log("Error", error)
-  console.log("Data", data)
-})
+if (!address) {
+	console.log("Please provide an address.")
+} else {
+	// Use callback chaining to call geocode AND THEN call forecast
+	geocode(address, (error, data) => {
+		if (error) {
+			return console.log(error)
+		}
+
+		forecast(data.latitude, data.longitude, (error, forecastData) => {
+			if (error) {
+				return console.log(error)
+			}
+
+			console.log(data.location)
+			console.log(forecastData)
+		})
+	})
+}
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,4 +1,4 @@
-const request = require('request')
+const request = require("request")
 
 /**
  * Requests weather data from weatherstack api for a given latitude and longitude
@@ -7,22 +7,28 @@ const request = require('request')
  * @param {*} callback function called once we have the latitude and longitude
  */
 const forecast = (latitude, longitude, callback) => {
-    const weatherstackURL = 'http://api.weatherstack.com/current?access_key=62ba8e98565a8d0fed54f9a8eb9efff2&query=' + latitude + ',' + longitude + '&units=f'
+	const weatherstackURL =
+		"http://api.weatherstack.com/current?access_key=62ba8e98565a8d0fed54f9a8eb9efff2&query=" +
+		latitude +
+		"," +
+		longitude +
+		"&units=f"
 
-    request({url: weatherstackURL, json: true}, (error, response) => {
-        if (error) {
-            callback('Could not access weather data!')
-        }
-        else if(response.body.error) {
-            callback('Could not find that location.')
-        }
-        else {
-            const temp = response.body.current.temperature
-            const feelsLike = response.body.current.feelslike
-            const descr = response.body.current.weather_descriptions[0]
-            callback(undefined, descr + '\nTemperature: ' + temp + '\nFeels like: ' + feelsLike)
-        }
-    })
+	request({ url: weatherstackURL, json: true }, (error, response) => {
+		if (error) {
+			callback("Could not access weather data!")
+		} else if (response.body.error) {
+			callback("Could not find that location.")
+		} else {
+			const temp = response.body.current.temperature
+			const feelsLike = response.body.current.feelslike
+			const descr = response.body.current.weather_descriptions[0]
+			callback(
+				undefined,
+				descr + "\nTemperature: " + temp + "\nFeels like: " + feelsLike
+			)
+		}
+	})
 }
 
 module.exports = forecast
